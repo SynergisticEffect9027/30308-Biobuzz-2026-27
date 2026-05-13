2971ded2f094156465bd249185fe709a37631f0e
Ava The Cool B] <ava.turpin17@gmail.com>
2026-05-13 12:24:03 -0500

    follow constant pedro pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -7,7 +7,25 @@ import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(5); //In kilograms, change to your robot's weight!!
+
+    public static MecanumConstants driveConstants = new MecanumConstants() //Change to your motors
+            .maxPower(1)
+            .rightFrontMotorName("rf")
+            .rightRearMotorName("rr")
+            .leftRearMotorName("lr")
+            .leftFrontMotorName("lf")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+
+    public static OTOSConstants localizerConstants = new OTOSConstants()//Change to your numbers
+            .hardwareMapName("otos")
+            .linearUnit(DistanceUnit.INCH)
+            .offset(new SparkFunOTOS.Pose2D(0, 0, Math.PI/2))
+            .angleUnit(AngleUnit.RADIANS);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
