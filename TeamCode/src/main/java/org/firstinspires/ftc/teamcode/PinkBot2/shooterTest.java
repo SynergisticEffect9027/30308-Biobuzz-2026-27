390cb711e4ccfbda9c070c356eecfe7c3207ec68
jack <jackmatthewfranklin@gmail.com>
2026-06-02 11:23:32 -0500

    added pink bot code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinkBot2/shooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinkBot2/shooterTest.java
@@ -10,6 +10,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 @TeleOp(name="shooterTest-pinkBot2", group="pink-bot2")
 public class shooterTest extends LinearOpMode{
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeftDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backRightDrive = null;
     private DcMotorEx shooterA;
     private DcMotorEx shooterB;
 
@@ -20,6 +24,17 @@ public class shooterTest extends LinearOpMode{
 
     @Override
     public void runOpMode() {
+
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeft");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeft");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRight");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
         lancherTimer = new ElapsedTime();
         shooterA = hardwareMap.get(DcMotorEx.class, "shooterA");
         shooterB = hardwareMap.get(DcMotorEx.class, "shooterB");
@@ -45,6 +60,38 @@ public class shooterTest extends LinearOpMode{
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
+                double max;
+
+                // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+                double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+                double lateral =  gamepad1.left_stick_x;
+                double yaw     =  gamepad1.right_stick_x;
+
+                // Combine the joystick requests for each axis-motion to determine each wheel's power.
+                // Set up a variable for each drive wheel to save the power level for telemetry.
+                double frontLeftPower  = axial + lateral + yaw;
+                double frontRightPower = axial - lateral - yaw;
+                double backLeftPower   = axial - lateral + yaw;
+                double backRightPower  = axial + lateral - yaw;
+
+                // Normalize the values so no wheel power exceeds 100%
+                // This ensures that the robot maintains the desired motion.
+                max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+                max = Math.max(max, Math.abs(backLeftPower));
+                max = Math.max(max, Math.abs(backRightPower));
+
+                if (max > 1.0) {
+                    frontLeftPower  /= max;
+                    frontRightPower /= max;
+                    backLeftPower   /= max;
+                    backRightPower  /= max;
+                }
+
+                frontLeftDrive.setPower(frontLeftPower);
+                frontRightDrive.setPower(frontRightPower);
+                backLeftDrive.setPower(backLeftPower);
+                backRightDrive.setPower(backRightPower);
+
                 double targetRPM = 0; //shooter
                 double ticksPerRev = 28;
                 double dis = 0;
