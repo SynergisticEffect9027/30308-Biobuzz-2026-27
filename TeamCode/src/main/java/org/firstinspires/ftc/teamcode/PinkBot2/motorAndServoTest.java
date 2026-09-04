f36596f04304f3334d8bae5c7f4ca8d44310ecda
jack <jackmatthewfranklin@gmail.com>
2026-09-04 16:29:55 -0500

    added pink bot code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinkBot2/motorAndServoTest.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.PinkBot2;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@TeleOp(name="motorTestServo", group="pink-bot2")
+public class motorAndServoTest extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor frontLeftDrive = null;
+
+    @Override
+    public void runOpMode() {
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontLeftDrive.setDirection(DcMotor.Direction.FORWARD);
+        Servo sickServo = hardwareMap.get(Servo.class, "servo");
+
+        waitForStart();
+        runtime.reset();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (gamepad1.b){
+                    frontLeftDrive.setPower(.5);
+                }
+                if(gamepad1.a){
+                    frontLeftDrive.setPower(0);
+                }
+                if (gamepad1.x){
+                    sickServo.setPosition(1);
+                }
+                if(gamepad1.y){
+                    sickServo.setPosition(0);
+                }
+            }
+        }
+    }
+}
